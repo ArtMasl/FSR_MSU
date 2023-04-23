b80e86028f3feae74b21bb523145ec7d96602fcd
ArtMasl <artmasl04@gmail.com>
2023-04-23 07:33:58 +0300

    ONLY BLUE

--- 1course.c
+++ 1course.c
@@ -122,7 +122,7 @@ int main() {
     for (int i = 1; i < w-1; i++){
         for (int j = 1; j < h-1; j++){
             get_pixel(i, j, &r, &g, &b, &a, picture, w);
-            set_pixel(i, j, r, g, 0, a, picture, w);
+            set_pixel(i, j, 0, 0, b, a, picture, w);
         }
     }
     // read file and convert it to 2D array
