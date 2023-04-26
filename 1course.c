123f3b5fe240c1a36742c0f06977c2f3851e69d8
ArtMasl <artmasl04@gmail.com>
2023-04-26 15:54:40 +0300

    NEW GAUSS

--- 1course.c
+++ 1course.c
@@ -27,125 +27,88 @@ void writePng(const char* filename, const unsigned char* image, unsigned width, 
   if(!error) {
     lodepng_save_file(png, pngsize, filename);
   }
- 
+
   /*if there's an error, display it*/
   if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
+
+ 
 
   free(png);
 }
 
-void get_pixel(int x, int y, int *r, int *g, int *b, int *a, char* image, int width )
-{
-
-   *r =  image[4 * width * y + 4 * x + 0];
-   *g =  image[4 * width * y + 4 * x + 1];
-   *b =  image[4 * width * y + 4 * x + 2];
-   *a =  image[4 * width * y + 4 * x + 3];
-
-   return;
+void preparation(char * od, int w, int h){
+    int i, j;
+    for (i=0; i<h; i++)
+           for (j=0; j<w; j++) {
+                if (od[w*i + j]<100) od[w*i + j]=0;
+                if (od[w*i + j]>168) od[w*i + j]=255;
+           }
+    return;
 }
 
-void set_pixel(int x, int y, int r, int g, int b, int a, char* image, int width )
-{
-
-   image[4 * width * y + 4 * x + 0] = r;
-   image[4 * width * y + 4 * x + 1] = g;
-   image[4 * width * y + 4 * x + 2] = b;
-   image[4 * width * y + 4 * x + 3] = a;
-
-   return;
-}
-int is_close(int r1, int g1, int b1, int a1, int r2, int g2, int b2, int a2) {
-    int e_r = 10;
-    int e_g = 10;
-    int e_b = 10;
-    int e_a = 10;
-    if ((fabs(r1 - r2)<e_r)&&(fabs(g1 - g2) < e_g)&&(fabs(b1 - b2) < e_b)&&(fabs(a1 - a2) < e_a)) return 1;
-    else return 0;
-   }
-
-
-int is_black(int r, int g, int b) {
-  // Here is the place for experiments and improvments
-      int gray=(r+g+b)/3;
-      if ( gray < 64 ) return 1;
-      else return 0;
+void fSobel(char * od, char * dc, int w, int h){
+        char x, y;
+        int i, j;
+        for (i=1; i < h-1; i++)
+           for (j=1; j < w-1; j++) {
+                x=(-1)*od[w*(i-1)+(j-1)]-2*od[w*i+(j-1)]-1*od[w*(i+1)+(j-1)]+1*od[w*(i-1)+(j+1)]+2*od[w*i+(j+1)]+1*od[w*(i+1)+(j+1)];
+                y=(-1)*od[w*(i-1)+(j-1)]-2*od[w*(i-1)+j]-1*od[w*(i-1)+(j+1)]+1*od[w*(i+1)+(j-1)]+2*od[w*(i+1)+j]+1*od[w*(i+1)+(j+1)];
+                dc[i*w+j]=sqrt(x*x + y*y);
+           }
+        return;
 }
 
-void preprocess_image_Gauss(char * image, int width, int height, int n) {
-        int x, y, i, j;
-        double g[3][3];
-        g[0][0] = g[0][2] = g[2][0] = g[2][2] =  1;
-        g[0][1] = g[1][0] = g[1][2] = g[2][1] =  4;
-        g[1][1] = 12;
-        for (x=1; x<=(width-2); x++)
-                for (y=1; y<=(height-2); y++)
-                        for (i=-1; i<=1; i++)
-                                for (j=-1; j<=1; j++)
-                                        image[n*(y+j)*width + n*i + n*x] += g[1-i][1-j];
-}
-  void preprocess_image_Sobel_x(char * image, int width, int height, int n) {
-        int x, y, i, j;
-        double s[3][3];
-        s[0][0] = s[2][0] =  -1;
-        s[0][2] = s[2][2] =  1;
-        s[1][0] = -2;
-        s[1][2] = 2;
-        s[0][1] = s[1][1] = s[2][1] =  0;
-        for (x=1; x<=(width-2); x++)
-                for (y=1; y<=(height-2); y++)
-                        for (i=-1; i<=1; i++)
-                                for (j=-1; j<=1; j++)
-                                        image[n*(y+j)*width + i + n*x] += s[1-i][1-j];
+void fGauss(char * od, char * dc, int w, int h){
+        char x, y;
+        int i, j;
+        for (i=1; i < h-1; i++)
+           for (j=1; j < w-1; j++) {
+                dc[w*i+j]=0.12*od[w*i+j]+0.12*od[w*(i+1)+j]+0.12*od[w*(i-1)+j];
+                 dc[w*i+j]=dc[w*i+j]+0.12*od[w*i+(j+1)]+0.12*od[w*i+(j-1)];
+                dc[w*i+j]=dc[w*i+j]+0.09*od[w*(i+1)+(j+1)]+0.09*od[w*(i+1)+(j-1)];
+                dc[w*i+j]=dc[w*i+j]+0.09*od[w*(i-1)+(j+1)]+0.09*od[w*(i-1)+(j-1)];
+           }
+        return;
 }
 
-void preprocess_image_Sobel_y(char * image, int width, int height, int n) {
-        int x, y, i, j;
-        double s[3][3];
-        s[0][0] = s[0][2] =  -1;
-        s[2][0] = s[2][2] =  1;
-        s[0][1] = -2;
-        s[2][1] = 2;
-        s[1][0] = s[1][1] = s[1][2] =  0;
-        for (x=1; x<=(width-2); x++)
-                for (y=1; y<=(height-2); y++)
-                        for (i=-1; i<=1; i++)
-                                for (j=-1; j<=1; j++)
-                                        image[n*(y+j)*width + i + n*x] += s[1-i][1-j];
-}
-void preprocess_image_Sobel_all(char * image_x, char * image_y, int width, int height, int n) {
+void colouring(char * dc, char * mcod, int w, int h, int n){
         int i;
-        for (i=0; i<n*width*height; i++) image_x[i] = sqrt(image_x[i]*image_x[i] + image_y[i]*image_y[i]);
+        int c[w*h];
+        for (i=0; i<w*h; i++) c[i]=dc[i];
+        for (i=1; i<w*h; i++) {
+                mcod[i*n]=80+c[i]+0.5*c[i-1];
+                mcod[i*n+1]=45+c[i];
+                mcod[i*n+2]=150+c[i];
+                mcod[i*n+3]=225;
+        }
+        return;
 }
+
 
 int exists(int i,int j,int width,int height){
     if ((i>0) && (i<height) && (j>0) && (j<width)) return 1;
     return 0;
 }
 
-void DFS(int i,int j,int w,int h,unsigned char* image, int* components,int adj_num) {
-    int r, g, b, a;
-    int r1, g1, b1, a1;
+void DFS(int i,int j,int w,int h,unsigned char* image, int* components,int adj_num)
+{
     components[w*i + j] = adj_num;
-    get_pixel(i, j, &r, &g, &b, &a, image, w);
-    if (exists(i, j-2, w, h)) {
-	get_pixel(i, j-2, &r1, &g1, &b1, &a1, image, w);
-        if ((is_close(r, g, b, a, r1, g1, b1, a1)) && !(components[w*i + (j-2)]))
+    if (exists(i, j-2, w, h))
+        if ((fabs(image[w*i + j] - image[w*i + (j-2)]) <= 10) && !(components[w*i + (j-2)]))
             DFS(i, j-2, w, h, image, components, adj_num);
-    }
 
-    if (exists(i-2, j+1, w, h)) {
-        get_pixel(i-2, j+1, &r1, &g1, &b1, &a1, image, w);
-        if ((is_close(r, g, b, a, r1, g1, b1, a1)) && !(components[w*(i-2) + (j+1)]))
-            DFS(i-2, j+1, w, h, image, components, adj_num);
-    }
-
-    if (exists(i+2, j+1, w, h)) {
-	get_pixel(i+2, j+1, &r1, &g1, &b1, &a1, image, w);
-        if ((is_close(r, g, b, a, r1, g1, b1, a1)) && !(components[w*(i+2) + (j+1)]))
-            DFS(i+2, j+1, w, h, image, components, adj_num);
-    }
  
+
+    if (exists(i-2, j+1, w, h))
+        if ((fabs(image[w*i + j] - image[w*(i-2) + (j+1)]) <= 10) && !(components[w*(i-2) + (j+1)]))
+            DFS(i-2, j+1, w, h, image, components, adj_num);
+
+
+    if (exists(i+2, j+1, w, h))
+        if ((fabs(image[w*i + j] - image[w*(i+2) + (j+1)]) <= 10) && !(components[w*(i+2) + (j+1)]))
+            DFS(i+2, j+1, w, h, image, components, adj_num);
+
+
 
     return;
 }
@@ -153,58 +116,53 @@ void DFS(int i,int j,int w,int h,unsigned char* image, int* components,int adj_n
 int main() {
 
     //char * filename = "C_ver1.png";
-    char * filename = "scull.png";
-    int w, h, i, j, c, k=0, adj_num=0;
-    int r, g, b, a, n=4;
-    int r1, g1, b1, a1;
+    char * filename = "Scull.png";
+    int w, h, i, j, k=0, adj_num=0, n=4;
     char * picture = loadPng(filename, &w, &h);
-    //char * picture_1 = loadPng(filename, &w, &h);
     if (picture == NULL){
         printf("I can not read the picture from the file %s. Error.\n", filename);
         return -1;
     }
 
-    //if (picture_1 == NULL){
-      //  printf("I can not read the picture from the file %s. Error.\n", filename);
-        //return -1;
-    //}
 
-    preprocess_image_Gauss(picture, w, h, n);
-
-    //preprocess_image_Sobel_x(picture, w, h, n);
-    //preprocess_image_Sobel_y(picture_1, w, h, n);
-    //preprocess_image_Sobel_all(picture, picture_1, w, h, n);
- 
-    for (int i = 0; i < w; i++){
-        for (int j = 0; j < h; j++){
-            get_pixel(i, j, &r, &g, &b, &a, picture, w);
-            if (is_black(r, g, b)) set_pixel(i, j, 0, 0, 0, a, picture, w);
-            else set_pixel(i, j, 255, 255, 255, a, picture, w);
-        }
+    char* image = (char*)malloc(w*h*sizeof(char));
+    for (i = 0; i < n*w*h; i+=n) {
+        image[k] = 0.299*picture[i] + 0.578*picture[i+1] + 0.114*picture[i+2];
+        k++;
     }
+
+    char* image_1 = image;
+
+    preparation(image, w, h);
+//    fSobel(image, image_1, w, h);
+    fGauss(image, image_1, w, h);
 
     int* comps = (int*)malloc((w*h)*sizeof(int));
     for (i=0; i < w*h; i++) comps[i] = 0;
 
-    for (i=2; i < h-1; i++)
-        for (j=2; j < w-1; j++)
+    for (i = 2; i < h-1; i++)
+        for (j = 2; j < w-1; j++)
             if (!comps[w*i + j]) {
                    adj_num++;
-                DFS(i, j, w, h, picture, comps, adj_num);
+                DFS(i, j, w, h, image, comps, adj_num);
             }
 
-    k=0;
-    for (i=0; i < n*w; i+=n)
-	for (j=0; j < n*h; j+=n) {
-            c = comps[k]%50 + comps[k]%37;
-	    set_pixel(i, j, (4*c -70), (3*c + 40), (5*c + 60), 255, picture, w);
-            k++;
-        }
+    char* data = (char*)malloc(n*w*h*sizeof(char));
+
+    k=1;
+    for (i=0; i < n*w*h; i+=n)
+    {
+        data[i] = 80+comps[k]+0.5*comps[k-1];
+        data[i+1] = 45+comps[k];
+        data[i+2] = 150+comps[k];
+        if (n==4) data[i+3] = 255;
+        k++;
+   }
 
     //char * new_image = "C_ver1-modified.png";
-    char * new_image = "scull-modified.png";
-    writePng(new_image, picture, w, h);
+    char * new_image = "Scull-modified.png";
+    writePng(new_image, data, w, h);
 
     return 0;
 }
-                                                              
+                                                               
