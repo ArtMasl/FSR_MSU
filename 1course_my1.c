bbaec7b10cafc31e45a15649441607fa171799cc
ArtMasl <artmasl04@gmail.com>
2023-04-27 20:26:49 +0300

    FROM MV

--- 1course_my1.c
+++ 1course_my1.c
@@ -39,7 +39,7 @@ void writePng(const char* filename, const unsigned char* image, unsigned width, 
 void preparation(char * od, int w, int h){
     int i, j;
     for (i=0; i<h; i++)
-           for (j=0; j<w; j++) {
+           for (j=0; i<w; j++) {
                 if (od[w*i + j]<100) od[w*i + j]=0;
                 if (od[w*i + j]>168) od[w*i + j]=255;
            }
@@ -64,7 +64,7 @@ void fGauss(char * od, char * dc, int w, int h){
         for (i=1; i < h-1; i++)
            for (j=1; j < w-1; j++) {
                 dc[w*i+j]=0.12*od[w*i+j]+0.12*od[w*(i+1)+j]+0.12*od[w*(i-1)+j];
-                dc[w*i+j]=dc[w*i+j]+0.12*od[w*i+(j+1)]+0.12*od[w*i+(j-1)];
+                 dc[w*i+j]=dc[w*i+j]+0.12*od[w*i+(j+1)]+0.12*od[w*i+(j-1)];
                 dc[w*i+j]=dc[w*i+j]+0.09*od[w*(i+1)+(j+1)]+0.09*od[w*(i+1)+(j-1)];
                 dc[w*i+j]=dc[w*i+j]+0.09*od[w*(i-1)+(j+1)]+0.09*od[w*(i-1)+(j-1)];
            }
@@ -84,84 +84,34 @@ void colouring(char * dc, char * mcod, int w, int h, int n){
         return;
 }
 
-
-int exists(int i,int j,int width,int height){
-    if ((i>0) && (i<height) && (j>0) && (j<width)) return 1;
-    return 0;
-}
-
-void DFS(int i,int j,int w,int h,unsigned char* image, int* components,int adj_num)
-{
-    components[w*i + j] = adj_num;
-    if (exists(i, j-2, w, h))
-        if ((fabs(image[w*i + j] - image[w*i + (j-2)]) <= 10) && !(components[w*i + (j-2)]))
-            DFS(i, j-2, w, h, image, components, adj_num);
-
- 
-
-    if (exists(i-2, j+1, w, h))
-        if ((fabs(image[w*i + j] - image[w*(i-2) + (j+1)]) <= 10) && !(components[w*(i-2) + (j+1)]))
-            DFS(i-2, j+1, w, h, image, components, adj_num);
-
-
-    if (exists(i+2, j+1, w, h))
-        if ((fabs(image[w*i + j] - image[w*(i+2) + (j+1)]) <= 10) && !(components[w*(i+2) + (j+1)]))
-            DFS(i+2, j+1, w, h, image, components, adj_num);
-
-    return;
-}
-
-
 int main() {
 
     //char * filename = "hamster.png";
     char * filename = "Scull.png";
-    int w, h, i, j, k=0, adj_num=0, n=4;
-    char * picture = loadPng(filename, &w, &h);
-    if (picture == NULL){
+    int w, h, i, k=0, n=4;
+    char * idata = loadPng(filename, &w, &h);
+    if (idata == NULL){
         printf("I can not read the picture from the file %s. Error.\n", filename);
         return -1;
     }
 
-
-    char* image = (char*)malloc(w*h*sizeof(char));
-    for (i = 0; i < n*w*h; i+=n) {
-        image[k] = 0.299*picture[i] + 0.578*picture[i+1] + 0.114*picture[i+2];
+    char* odata = (char*)malloc(w*h*sizeof(char));
+    char* mcodata = (char*)malloc(w*h*sizeof(char)); 
+    for (i=0; i < w*h*n; i+=n){
+        odata[k]=0.299*idata[i]+0.578*idata[i+1]+0.114*idata[i+2];
         k++;
     }
 
-    char* image_1 = image;
+    char * datacopy = odata;
 
-    char* data = (char*)malloc(n*w*h*sizeof(char));
-
-    preparation(image, w, h);
-    fGauss(image, image_1, w, h);
-   // fSobel(image, image_1, w, h);
-    colouring(image_1, data, w, h, n);
-   /* int* comps = (int*)malloc((n*w*h)*sizeof(int));
-    for (i=0; i < w*h; i++) comps[i] = 0;
-
-    for (i = 2; i < h-1; i++)
-        for (j = 2; j < w-1; j++)
-            if (!comps[w*i + j]) {
-                   adj_num++;
-                DFS(i, j, w, h, image_1, comps, adj_num);
-            }
-
-    k=1;
-    for (i=0; i < n*w*h; i+=n)
-    {
-        data[i] = 80+comps[k]+0.5*comps[k-1];
-        data[i+1] = 45+comps[k];
-        data[i+2] = 150+comps[k];
-        if (n==4) data[i+3] = 255;
-        k++;
-   } */
+   // preparation(odata, w, h);
+   // fSobel(odata, datacopy, w, h);
+    //fGauss(odata, datacopy, w, h);
+    //colouring(datacopy, mcodata, w, h, n);
 
     //char * new_image = "hamster-modified.png";
     char * new_image = "Scull-modified.png";
-    writePng(new_image, data, w, h);
+    writePng(new_image, mcodata, w, h);
 
     return 0;
 }
-                                                               
