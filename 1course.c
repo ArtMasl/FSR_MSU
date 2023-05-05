e1bfe681d012e93f30b74cbcd69544ce79a29078
ArtMasl <artmasl04@gmail.com>
2023-05-05 10:23:18 +0300

    SGSC

--- 1course.c
+++ 1course.c
@@ -160,17 +160,16 @@ int main() {
     char* image_0 = (char*)malloc(w*h*sizeof(char));
     char* image_1 = (char*)malloc(w*h*sizeof(char));
     char* image_2 = (char*)malloc(w*h*sizeof(char));
-    char* image_3 = (char*)malloc(w*h*sizeof(char));
+    //char* image_3 = (char*)malloc(w*h*sizeof(char));
     //char* image_4 = (char*)malloc(w*h*sizeof(char));
    // char* image_5 = (char*)malloc(w*h*sizeof(char));
    // char* image_6 = (char*)malloc(w*h*sizeof(char));
     char* data = (char*)malloc(n*w*h*sizeof(char));
 
     Sobel(image, image_0, w, h);
-    preparation(image_0, w, h);
-    Sobel(image_0, image_2, w, h);
-    Gauss(image_2, image_3, w, h);
-    Sobel(image_3, image_1, w, h);
+    Gauss(image_0, image_2, w, h);
+    Sobel(image_2, image_1, w, h);
+    preparation(image_1, w, h);
     colouring(image_1, data, w, h, n);
   /*  int* comps = (int*)malloc((n*w*h)*sizeof(int));
     for (i=0; i < w*h; i++) comps[i] = 0;
