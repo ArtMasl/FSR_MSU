c2132b0217f82a2f72687cd5b7a58117ae5ab006
ArtMasl <artmasl04@gmail.com>
2023-05-11 16:23:40 +0300

    davv

--- 1course.c
+++ 1course.c
@@ -27,7 +27,7 @@ void writePng(const char* filename, const unsigned char* image, unsigned width, 
   free(png); 
 } 
  
-void preparation(unsigned char *omat, int w, int h){ 
+void preparation(char *omat, int w, int h){ 
     int i, j;
     for(i=2;i<h-1;i++) 
         for(j=2;j<w-1;j++){ 
@@ -39,7 +39,8 @@ void preparation(unsigned char *omat, int w, int h){
     return; 
 } 
  
-void Gauss(unsigned char *omat, unsigned char *d, int w, int h){     int i, j; 
+void Gauss(char *omat, char *d, int w, int h){     
+     int i, j; 
      for(i=2;i<h-1;i++) 
         for(j=2;j<w-1;j++){ 
             d[w*i+j]=0.12*omat[w*i+j]+0.12*omat[w*(i+1)+j]+0.12*omat[w*(i-1)+j]; 
@@ -50,7 +51,7 @@ void Gauss(unsigned char *omat, unsigned char *d, int w, int h){     int i, j;
    return; 
 } 
  
-void colouring(unsigned char *dmat, unsigned char *mpicture, int w, int h){
+void colouring(char *dmat, char *mpicture, int w, int h){
     int i;	
     for(i=1;i<w*h;i++) { 
         mpicture[i*4]=87+dmat[i]+0.5*dmat[i-1]; 
@@ -72,15 +73,15 @@ int main() {
         return -1; 
     } 
  
-    unsigned char* image = (unsigned char*)malloc(h*w*sizeof(unsigned char)); 
+    char* image = (char*)malloc(h*w*sizeof(char)); 
     
     for(i=0;i<n*h*w;i+=n){ 
  	image[k]=0.299*picture[i]+0.587*picture[i+1]+0.114*picture[i+2]; 
          k++; 
     } 
 
-    unsigned char* image_1 = (unsigned char*)malloc(h*w*sizeof(unsigned char));
-    unsigned char* data = (unsigned char*)malloc(n*h*w*sizeof(unsigned char));
+    char* image_1 = (char*)malloc(h*w*sizeof(char));
+    char* data = (char*)malloc(n*h*w*sizeof(char));
 
     preparation(image, w, h); 
     Gauss(image, image_1, w, h); 
