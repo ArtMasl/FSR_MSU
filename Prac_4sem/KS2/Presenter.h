e1c65c7e76cb9196a67865ce0d3f2f733746b9db
ArtMasl <126054477+ArtMasl@users.noreply.github.com>
2024-04-23 17:02:04 +0300

    Create Presenter.h

---
+++ Prac_4sem/KS2/Presenter.h
@@ -0,0 +1,21 @@
+#include "Solver.h"
+#include<filesystem>
+#include<fstream>
+namespace  fs = std::filesystem;
+using namespace std;
+
+//function to get list of files in required directory
+vector<string> filesindir(const fs::path& dir) {
+	vector<string> files;
+	for (auto& p : fs::recursive_directory_iterator(dir)) {
+		if (fs::is_regular_file(p)) {
+			files.push_back(p.path().string());
+		}
+	}
+	return files;
+}
+//function to fill results
+void fill_result(int& wt, ofstream& out,string& file_name) {
+	out << file_name << " ";
+	out << wt << '\n';
+}
