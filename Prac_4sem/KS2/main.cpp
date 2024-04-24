736e5cae46b2488ad42dd2199d0358adba1a6012
ArtMasl <126054477+ArtMasl@users.noreply.github.com>
2024-04-24 21:52:29 +0300

    Update main.cpp

--- Prac_4sem/KS2/main.cpp
+++ Prac_4sem/KS2/main.cpp
@@ -6,11 +6,9 @@ int main() {
     fs::path dir{ "data" };
     fs::path pathtodata = cur_p / dir;
     files = filesindir(pathtodata);
-    //ofstream out("result.txt");
     for (int file = 0; file < files.size(); file++) {
         ifstream in(files[file]);
         int K, n, val, wt;
-        //creation of given list of items
         vector<item> items; 
         in >> n >> K;
         for (int i = 0; i < n; i++) {
@@ -20,15 +18,12 @@ int main() {
         }
         vector<int> used(n, 0);
         vector<item> sorted_items = sort_vector(items);
-        int total_profit = knapsack(sorted_items, used, n, K);
-        //cout << total_profit << endl;
-        vector<int> initial_order = initial_element_order(used, n, items, sorted_items);// uncommenting will reveal the used items for filling knapsack
-        //show1d_matrix<int>(initial_order);
+        int sum = Solve(sorted_items, used, n, K);
+        vector<int> initial_order = initial_element_order(used, n, items, sorted_items);
 	files[file].erase(0, 33);
 	ofstream out("result_" + files[file] + ".txt");
-        fill_result(total_profit, out, initial_order);
+        fill_result(sum, out, initial_order);
         in.close();
 	out.close();
     }
-    //out.close();  
 }
