7b9c0db196ebc8a6be449e909efb01b9c33c32da
ArtMasl <126054477+ArtMasl@users.noreply.github.com>
2024-04-24 20:06:01 +0300

    Update Presenter.h

--- Prac_4sem/KS2/Presenter.h
+++ Prac_4sem/KS2/Presenter.h
@@ -1,8 +1,6 @@
 #include "Solver.h"
-#include<string>
 #include<filesystem>
 #include<fstream>
-#include<sstream>
 namespace  fs = std::filesystem;
 using namespace std;
 
@@ -17,12 +15,7 @@ vector<string> filesindir(const fs::path& dir) {
 	return files;
 }
 //function to fill results
-void fill_result(int& wt, ofstream& out, string& file_name, vector<int>& used) {
-	//ofstream out("result_" + file_name + ".txt");
-	out << file_name << '\n';
-	out << wt << 0 << '\n';
-	for (int i=0; i<used.size(); i++)
-		out << used[i] << " ";
-	out << "\n\n";
-	//out.close();
+void fill_result(int& wt, ofstream& out,string& file_name) {
+	out << file_name << " ";
+	out << wt << '\n';
 }
