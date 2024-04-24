977bc0b48eb68e6fa07ad50166798fe287b295c6
ArtMasl <126054477+ArtMasl@users.noreply.github.com>
2024-04-24 20:08:35 +0300

    Update main.cpp

--- Prac_4sem/KS2/main.cpp
+++ Prac_4sem/KS2/main.cpp
@@ -9,11 +9,6 @@ int main() {
     ofstream out("result.txt");
     for (int file = 0; file < files.size(); file++) {
         ifstream in(files[file]);
-        //ifstream in;
-        //in.open(files[file]);
-        //ofstream out("result_" + files[file] + ".txt");
-        //ofstream out;
-        //out.open("result_" + files[file] + ".txt");
         int K, n, val, wt;
         //creation of given list of items
         vector<item> items; 
@@ -29,8 +24,8 @@ int main() {
         cout << total_profit << endl;
         /*vector<int> initial_order = initial_element_order(used, n, items, sorted_items);// uncommenting will reveal the used items for filling knapsack
         show1d_matrix<int>(initial_order);*/
-        fill_result(total_profit, out, files[file], used);
+        fill_result(total_profit, out, files[file]);
         in.close();
     }
-    out.close();
+    out.close();  
 }
